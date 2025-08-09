c1e5f814df13c08d42734aa5e47572423b4188cd
fajarrizkyhidayat <fajarrizkyhidayat@gmail.com>
2025-08-09 09:27:13 +0700

    update test

--- src/app/test/layout.tsx
+++ src/app/test/layout.tsx
@@ -2,7 +2,7 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import { ThemeProvider } from "@mui/material/styles";
-import theme from "../../theme";
+import theme from "@/views/theme";
 import { AppBar, Toolbar } from "@mui/material";
 import Header from "@/views/components/base/headers/header";
 
