7ce65285f88a09c5e0bdb13f557615ce4b71672c
fajarrizkyhidayat <fajarrizkyhidayat@gmail.com>
2025-08-11 10:51:47 +0700

    feat(slider):try blob

--- src/schema/slider.schema.ts
+++ src/schema/slider.schema.ts
@@ -7,7 +7,7 @@ export const createSliderSchema = z.object({
   caption: z.string().optional().nullish(),
   link: z.string().optional().nullish(),
   sortOrder: z.coerce.number().int().optional().nullish(),
-  file: z.file(),
+  file: z.instanceof(Blob).nonoptional(),
   status: z
     .enum(Object.values(StatusType) as [string, ...string[]])
     .default(StatusType.inactive),
@@ -17,7 +17,7 @@ export type CreateSliderSchema = z.infer<typeof createSliderSchema>;
 
 export const updateSliderSchema = createSliderSchema.extend({
   id: z.coerce.number().int().nonoptional(),
-  file: z.file().optional().nullish(),
+  file: z.instanceof(Blob).optional().nullish(),
 });
 
 export type UpdateSliderSchema = z.infer<typeof updateSliderSchema>;
