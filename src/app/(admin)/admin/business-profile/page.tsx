3e1d8bd96a5dfb4271bb4ed18110e47dc46c4ebb
fajarrizkyhidayat <fajarrizkyhidayat@gmail.com>
2025-08-20 18:34:59 +0700

    updte

--- src/app/(admin)/admin/business-profile/page.tsx
+++ src/app/(admin)/admin/business-profile/page.tsx
@@ -1,6 +1,12 @@
+"use client";
 import React from "react";
-import AdminBusiness from "@/views/pages/busines-profile/AdminBusiness";
+import dynamic from "next/dynamic";
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import("@/views/pages/busines-profile/AdminBusiness"),
+  { ssr: false }
+);
 
 export default function Page() {
-  return <AdminBusiness />;
+  return <DynamicComponentWithNoSSR />;
 }
