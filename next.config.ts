4979cc930327e02b7fa1a179b95cd962bdffa376
fajarrizkyhidayat <fajarrizkyhidayat@gmail.com>
2025-08-20 21:50:30 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   images: {
-    remotePatterns: [new URL("http://localhost:4000/**")],
+    remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/**`)],
   },
   headers: async () => {
     return [
