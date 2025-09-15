212608328c637ec91ad4f213e9932292a1d7777c
fajarrizkyhidayat <fajarrizkyhidayat@gmail.com>
2025-09-15 21:00:24 +0700

    update

--- src/http/services/interactive.service.ts
+++ src/http/services/interactive.service.ts
@@ -12,28 +12,28 @@ export const createContactClick = (payload: CreateContacClickSchema) => {
 
 export const createBusinessView = async (payload: CreateBusinessViewSchema) => {
   let count = 0;
-  if (payload.visitorId) {
-    count = await prisma.businessView.count({
-      where: { visitorId: payload.visitorId, profileId: payload.profileId },
-    });
-  }
+  // if (payload.visitorId) {
+  //   count = await prisma.businessView.count({
+  //     where: { visitorId: payload.visitorId, profileId: payload.profileId },
+  //   });
+  // }
 
-  if (count > 0) {
-    return { createdAt: dayjs().format() };
-  }
+  // if (count > 0) {
+  //   return { createdAt: dayjs().format() };
+  // }
   return prisma.businessView.create({ data: payload });
 };
 
 export const createServiceView = async (payload: CreateServiceViewSchema) => {
   let count = 0;
-  if (payload.visitorId) {
-    count = await prisma.serviceView.count({
-      where: { visitorId: payload.visitorId, serviceId: payload.serviceId },
-    });
-  }
+  // if (payload.visitorId) {
+  //   count = await prisma.serviceView.count({
+  //     where: { visitorId: payload.visitorId, serviceId: payload.serviceId },
+  //   });
+  // }
 
-  if (count > 0) {
-    return { createdAt: dayjs().format() };
-  }
+  // if (count > 0) {
+  //   return { createdAt: dayjs().format() };
+  // }
   return prisma.serviceView.create({ data: payload });
 };
