b75dee82701faa571572f9b166cecc3d39b7b984
fajarrizkyhidayat <fajarrizkyhidayat@gmail.com>
2025-09-16 01:13:30 +0700

    update

--- src/views/services/image.service.ts
+++ src/views/services/image.service.ts
@@ -2,7 +2,7 @@ import { EventSend } from "ezhooks";
 import * as utils from "ezhooks/lib/utils";
 
 export const imageUrl = {
-  image: `${process.env.NEXT_PUBLIC_BASE_URL}/api/image`,
+  image: `${process.env.NEXT_PUBLIC_BASE_URL}/api/image/upload`,
   gallery: `${process.env.NEXT_PUBLIC_BASE_URL}/api/image/gallery`,
 };
 
